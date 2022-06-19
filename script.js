690d4c70bb5888b543c94ff8572750b37950313c
Artem Bystritskyi <artemb0607@gmail.com>
2022-06-19 19:04:46 +0300

    Lab 6

--- script.js
+++ script.js
@@ -1,59 +1,122 @@
-// 1 завдання
-const itemBloggingEl = document.getElementById("blogging");
-itemBloggingEl.onclick = changeElementStyles;
+// Завдання 1
 
-function changeElementStyles() {
-  itemBloggingEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  itemBloggingEl.style.color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const formEl = document.querySelector(".info-form");
 
-const itemProgrammingEl = document.querySelector(".hobbies-list_programming");
+const inputInitialsEl = document.querySelector("#initials");
+const inputFacultyEl = document.querySelector("#faculty");
+const inputDateEl = document.querySelector("#date");
+const inputAddressEl = document.querySelector("#address");
+const inputEmailEl = document.querySelector("#mail");
+const submitBtnEl = document.querySelector("button[type=submit]");
 
-itemProgrammingEl.addEventListener("click", () => {
-  itemProgrammingEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  itemProgrammingEl.style.color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const dataInitialsEl = document.querySelector("#data-initials");
+const dataFacultyEl = document.querySelector("#data-faculty");
+const dataDateEl = document.querySelector("#data-date");
+const dataAddressEl = document.querySelector("#data-address");
+const dataEmailEl = document.querySelector("#data-mail");
+
+const initialsValidation = /[А-Я-ІЄ][а-я-іє]+\s+[А-Я-ІЄ]\.\s+[А-Я-ІЄ]\./;
+const facultyValidation = /^[А-Я-ІЄ]{4}$/;
+const dateValidation = /^\d{2}\.\d{2}\.\d{4}$/;
+const addressValidation = /[м]\.\s[А-Я-ІЄ][а-я-ієї]+/;
+const emailValidation = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const target = e.target;
+
+  if (
+    inputInitialsEl.classList.contains("success-input") &&
+    inputFacultyEl.classList.contains("success-input") &&
+    inputDateEl.classList.contains("success-input") &&
+    inputAddressEl.classList.contains("success-input") &&
+    inputEmailEl.classList.contains("success-input")
+  ) {
+    dataInitialsEl.textContent = `ПІБ: ${target[0].value}`;
+    dataFacultyEl.textContent = `Група: ${target[1].value}`;
+    dataDateEl.textContent = `Дата народження: ${target[2].value}`;
+    dataAddressEl.textContent = `Адреса: ${target[3].value}`;
+    dataEmailEl.textContent = `Пошта: ${target[4].value}`;
+  } else {
+    alert("Please enter correct values!");
+  }
 });
 
-// 2 завданя
-const btnAddEl = document.querySelector(".btnAdd");
-const btnIncreaseEl = document.querySelector(".btnIncrease");
-const btnDecreaseEl = document.querySelector(".btnDecrease");
-const btnDeleteEl = document.querySelector(".btnDelete");
-const linkEl = document.querySelector(".img-link");
-const imgEl = document.querySelector(".img");
-
-btnAddEl.addEventListener("click", () => {
-  if (!linkEl.classList.contains("hidden")) {
-    alert("This photo is already on your site!");
+inputInitialsEl.addEventListener("input", (e) => {
+  if (e.target.value.length === 0) {
+    e.target.classList.remove("error-input");
+  } else if (initialsValidation.test(e.target.value)) {
+    e.target.classList.add("success-input");
+    e.target.classList.remove("error-input");
+  } else {
+    e.target.classList.remove("success-input");
+    e.target.classList.add("error-input");
   }
-
-  linkEl.classList.remove("hidden");
 });
 
-btnIncreaseEl.addEventListener("click", () => {
-  let imgSize = imgEl.clientWidth;
-
-  if (linkEl.classList.contains("hidden")) {
-    alert("Add the photo to the site!");
+inputFacultyEl.addEventListener("input", (e) => {
+  if (facultyValidation.test(e.target.value)) {
+    e.target.classList.add("success-input");
+    e.target.classList.remove("error-input");
+  } else {
+    e.target.classList.remove("success-input");
+    e.target.classList.add("error-input");
   }
-
-  imgEl.style.width = imgSize + 50 + "px";
 });
 
-btnDecreaseEl.addEventListener("click", () => {
-  if (linkEl.classList.contains("hidden")) {
-    alert("Add the photo to the site!");
+inputDateEl.addEventListener("input", (e) => {
+  if (dateValidation.test(e.target.value)) {
+    e.target.classList.add("success-input");
+    e.target.classList.remove("error-input");
+  } else {
+    e.target.classList.remove("success-input");
+    e.target.classList.add("error-input");
   }
-
-  let imgSize = imgEl.clientWidth;
-
-  imgEl.style.width = imgSize - 50 + "px";
 });
 
-btnDeleteEl.addEventListener("click", () => {
-  if (linkEl.classList.contains("hidden")) {
-    alert("This photo is already deleted from your site!");
+inputAddressEl.addEventListener("input", (e) => {
+  if (addressValidation.test(e.target.value)) {
+    e.target.classList.add("success-input");
+    e.target.classList.remove("error-input");
+  } else {
+    e.target.classList.remove("success-input");
+    e.target.classList.add("error-input");
   }
+});
 
-  linkEl.classList.add("hidden");
+inputEmailEl.addEventListener("input", (e) => {
+  if (emailValidation.test(e.target.value)) {
+    e.target.classList.add("success-input");
+    e.target.classList.remove("error-input");
+  } else {
+    e.target.classList.remove("success-input");
+    e.target.classList.add("error-input");
+  }
+});
+
+// Завдання 2
+
+const tableCellEl = document.querySelector(".my-variant");
+const allTableCells = document.querySelectorAll("td");
+const colorPickerEl = document.querySelector(".color-picker");
+let color = "";
+
+tableCellEl.addEventListener("mouseover", (e) => {
+  e.target.style.backgroundColor = `#${parseInt(Math.random() * 0xffffff)
+    .toString(16)
+    .padStart(6, "0")}`;
+});
+
+colorPickerEl.addEventListener("change", (e) => {
+  tableCellEl.style.backgroundColor = e.target.value;
+  color = e.target.value;
+});
+
+tableCellEl.addEventListener("dblclick", (e) => {
+  allTableCells.forEach((element) => {
+    if (!element.classList.contains("my-variant")) {
+      element.style.backgroundColor = color;
+    }
+  });
 });
